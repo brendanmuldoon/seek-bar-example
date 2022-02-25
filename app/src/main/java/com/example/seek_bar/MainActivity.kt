55b64194b2d2ab9450660e3478a79d34cdefbbf0
612847413 <brendan.muldoon@bt.com>
2022-02-25 16:02:50 +0000

    adding seekbar code

--- app/src/main/java/com/example/seek_bar/MainActivity.kt
+++ app/src/main/java/com/example/seek_bar/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.seek_bar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.seek_bar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MyViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initBinding()
+    }
+
+    private fun initBinding() {
+        val activityMainBinding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this,
+            R.layout.activity_main)
+
+        viewModel = ViewModelProvider.AndroidViewModelFactory(application)
+            .create(MyViewModel::class.java)
+
+        activityMainBinding.viewmodel = viewModel
+        activityMainBinding.lifecycleOwner = this
     }
 }
---
+++ app/src/main/java/com/example/seek_bar/MyViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.seek_bar
+
+import androidx.databinding.ObservableField
+import androidx.lifecycle.ViewModel
+
+class MyViewModel : ViewModel() {
+    var seekBar1 = ObservableField<Int>()
+}
